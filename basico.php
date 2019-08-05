918ae5097ae2caa0ad4e93df10ab65d00b29aaf8
Felipe R S Abbud <git@soufelipe.com>
2019-08-05 15:59:31 -0300

    Tipo de variáveis e mostrando informações na tela.

--- basico.php
+++ basico.php
@@ -1,5 +1,18 @@
 <?php
-  // Váriaveis - onde guardamos informações para serem usadas em todo o sistema.
+  // Variáveis - onde guardamos informações para serem usadas em todo o sistema.
   $nome = "Felipe";
 
+  //Tipos de variáveis
+  $nome = "Claudia"; //String
+  $numero = 300;//Integer
+  $float = 3.5;//Float
+  $boolean = true;// true or false
+  $array = (2, 3, "Felipe", true);//conjunto de informações em uma única variável
+
+
+  //Mostrando informações na tela.
+  echo "Hello World!";
+
+  //Concatenando informações de variáveis com texto normal.
+  echo "Meu nome é: " .$nome. ".";
 
