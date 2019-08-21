54f5eeb5b671d20981562212dcb6ac3096a72818
Felipe R S Abbud <git@soufelipe.com>
2019-08-21 17:19:13 -0300

    Curso Composer Aula 1 - Instalação.

---
+++ Composer/index.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Composer aula 1 - Instalando
+- baixar instalador no site do composer;
+- composer --version para ver se foi instalado;
+- gerenciador de dependencias de projetos PHP;
+- instalação no projeto:
+  - fizemos o composer init na pasta do projeto;
+  - colocamos nosso nome (github) e o nome do projeto;
+  - colocamos a descrição do pacote;
+  - o nome do autor. Se usamos o git no projeto, o autor por padrão é o usuáro do git que que está sendo usado;
+  - Minimum Stability: em branco. informa se quer pacotes em fase de testes ou estáveis;
+  - package type: library | project | metapackage | composer-plugin;
+  - license - deixar em branco.
+
+Composer aula 2 - Gerenciando dependências
+- buscar os pacotes no site packagist.org, repositório central mas não único;
+- 
+
+
+*/
