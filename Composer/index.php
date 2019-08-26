51a7eb448366c23030838cb6b7bea41af0b22df9
Felipe R S Abbud <git@soufelipe.com>
2019-08-26 13:53:25 -0300

    Composer Aula 3 - Configuração do autoload, classmap e files.

--- Composer/index.php
+++ Composer/index.php
@@ -22,7 +22,30 @@ Composer aula 2 - Gerenciando dependências
 - separamos em uma pasta src nosso código das classes para organizar melhor.
 
 Composer Aula 3 - Entendendo o Autoload
-- site PHP-fig-org.org
-- 
+- site PHP-fig-org.org para ver os padrões PSR-4
+- configurando o autoload no json:
+  "autoload": {
+        "psr-4": {
+            "namespacepadrão\\" : "diretório onde estão nossas classes" 
+        }
+  - depois atualizamos o autoload com: composer dump-autoload
+  
+  Classmap - informa que existe uma classe em determinado diretório
+  - colocamos o classmap no arquivo json dentro do autoload:
+        "classmap": [
+            "./Teste.php"
+        ],
+  - faremos o composer dump-autoload para atualizar;
+  - informa o autoload que temos que carregar o arquivo escrito no array classmap.
+  
+  Files - informa que existe arquivos isolados para serem carregados pelo autoload
+  - colocamos o código no json:
+        "files": [
+            "arquivoDefunções.php"
+        ],
+  - faremos o composer dump-autoload;
+  - informa o autoload que temos que carregar esse arquivo para usarmos.
+
+
 
 */
